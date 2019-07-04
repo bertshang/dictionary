6dd5c243015496ccdb7bb6872b59c317a6ce1794
bertalex <359352960@qq.com>
2019-07-04 14:59:35 +0000

    Apply fixes from StyleCI

--- src/Models/Dictinfo.php
+++ src/Models/Dictinfo.php
@@ -1,61 +1,78 @@
 <?php
 
+/*
+ * This file is part of the bertshang/dictionary.
+ *
+ * (c) bertshang <359352960@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
 namespace Bertshang\Dictionary\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
 /**
- * Class Dictinfo
- * @package Bertshang\Dictionary
+ * Class Dictinfo.
  */
-class Dictinfo extends Model {
-
+class Dictinfo extends Model
+{
     const STATUS_ON = 0;
+
     const STATUS_OFF = 1;
+
     public $timestamps = true;
 
     protected static function boot()
     {
         parent::boot();
 
-        static::deleting(function() {
+        static::deleting(function () {
             clearCache();
         });
 
-        static::saving(function() {
+        static::saving(function () {
             clearCache();
         });
 
-        static::creating(function() {
+        static::creating(function () {
             clearCache();
         });
 
-        static::updating(function() {
+        static::updating(function () {
             clearCache();
         });
     }
+
     /**
      * @return mixed
      */
-    public function type() {
+    public function type()
+    {
         return $this->belongsTo(Dicttype::class);
     }
 
     /**
      * @param $name
+     *
      * @return mixed
      */
-    public function exists($name) {
+    public function exists($name)
+    {
         return self::where('name', $name)->exists();
     }
 
     /**
      * @param array $data
+     *
      * @return bool
      */
-    public function add(array $data) {
+    public function add(array $data)
+    {
         try {
             self::create($data);
+
             return true;
         } catch (\Exception $e) {
             return false;
@@ -65,22 +82,22 @@ class Dictinfo extends Model {
     /**
      * @param $id
      * @param $data
+     *
      * @return bool
      */
-    public function edit($id, $data) {
+    public function edit($id, $data)
+    {
         try {
             self::where('id', $id)->update($data);
 
             return true;
         } catch (\Exception $e) {
-
             return false;
         }
     }
 
-    public function remove($name) {
+    public function remove($name)
+    {
         return self::where('value', $name)->delete();
     }
-
-
 }
--- src/helpers.php
+++ src/helpers.php
@@ -1,17 +1,25 @@
 <?php
-if (! function_exists('getKey')) {
 
+/*
+ * This file is part of the bertshang/dictionary.
+ *
+ * (c) bertshang <359352960@qq.com>
+ *
+ * This source file is subject to the MIT license that is bundled
+ * with this source code in the file LICENSE.
+ */
+
+if (!function_exists('getKey')) {
     function getKey($key, $staus = false)
     {
-
         return app('dictionary')->getKey($key, $staus);
     }
 }
 
-if (! function_exists('clearCache')) {
+if (!function_exists('clearCache')) {
     /**
      * @return mixed
-     * 清除缓存
+     *               清除缓存
      */
     function clearCache()
     {
@@ -19,16 +27,14 @@ if (! function_exists('clearCache')) {
     }
 }
 
-if (! function_exists('getAll')) {
-
+if (!function_exists('getAll')) {
     function getAll()
     {
         return app('dictionary')->getAll();
     }
 }
 
-if (! function_exists('getTypes')) {
-
+if (!function_exists('getTypes')) {
     function getTypes()
     {
         return app('dictionary')->getTypes();
